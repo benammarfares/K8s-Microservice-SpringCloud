a0fda7ab12685539067caee72aaf47a5e08baf8b
Fares <benammar.Fares@esprit.tn>
2024-08-21 10:46:44 +0100

    .

--- assurance/src/main/java/com/fares/assurance/AssuranceService.java
+++ assurance/src/main/java/com/fares/assurance/AssuranceService.java
@@ -27,7 +27,7 @@ public class AssuranceService {
          var assuranceClaim = assuranceRepository.findById(id).orElse(AssuranceClaim.builder()
                  .claimName("NOT_FOUND")
                  .claimDate(null)
-                 .claimDescription("No description available sorry")
+                 .claimDescription("No description")
                  .claimAmount(0.0)
                  .claimStatus("Unknown")
                  .claimProcessedDate(null)
